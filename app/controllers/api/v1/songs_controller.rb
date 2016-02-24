3b8eb6b4b800a380dd5a4afc6dfbc7fe387c1c14
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-02-24 18:22:45 -0500

    use embedded record mixin on ember side to create new song with assoc. artists and album

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -9,6 +9,22 @@ module Api
         song = Song.find(params[:id])
         render json: song
       end
+
+      def create
+        binding.pry
+        song = Song.new(song_params)
+        song.album = Album.find(params[:song][:album])
+        params[:song][:artists].each do |artist_id|
+          song.artists << Artist.find(artist_id)
+        end
+        render json: song
+      end
+
+      private
+
+        def song_params
+          params.require(:song).permit(:name)
+        end
     end
   end
 end
